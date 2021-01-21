31a1ac2b8da20bdc54e058b621acf9a935ad9649
Bordiuh <55415864+Bordiuh@users.noreply.github.com>
2021-01-21 15:11:30 +0200

    React app

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React,{Fragment} from 'react';
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Мое первое приложение на React</h1>
+            <p className="lead">Версия приложения <strong>1.0.2</strong></p>
+        </div>
+    </div>
+)
